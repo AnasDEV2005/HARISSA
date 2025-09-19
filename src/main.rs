a11270ce65c896606e2719aece7cf204c3cb6219
Anas <anas.siraj.souabni@gmail.com>
2025-09-19 19:25:50 +0300

    redone layout

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,4 @@
-use clap::Parser;
-mod scanner;
-mod parser;
-mod syntaxtree;
-mod transpiler;
-mod util;
-mod token;
-
-
-use scanner::Scanner;
-use token::Token;
-
-/// Simple TELL compiler (Phase 1 skeleton).
-#[derive(Parser)]
-#[command(author, version, about)]
-struct Cli {
-    /// Path to the .tell source file
-    source: String,
-}
-
 fn main() {
-    let source = "let x = 5 + 3";
-    let mut scanner = Scanner::new(source);
-
-    loop {
-        let token = scanner.next_token();
-        println!("{:?}", token);
-        if token == Token::EOF {
-            break;
-        }
-    }
+    println!("Za Warudo")
 }
 
